21a40d1412ba14626d62ee0ce0f53320b0ce104e
Owen Gombas <owen.gombas@gmail.com>
2018-09-06 19:23:13 +0200

    Improvements

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const pupp = require('puppeteer')
-const questions = require('./data/questions')
+const questions = require('.questions')
 
 const name = '4INF-HP-M'
 const delay = 0
+let actualQuestion = null
 
 const main = async () => {
   const browser = await pupp.launch({headless: false})
   const quizPage = await browser.newPage()
-  await quizPage.goto('https://www.admin.ch/gov/fr/accueil/conseil-federal/quiz.html', {waitUntil: 'networkidle2'})
+  await quizPage.goto('https://www.onlinequizcreator.com/fr/jeu-concours-sur-le-conseil-federal/quiz-342457', {waitUntil: 'networkidle2'})
   
-  // Select the game frame
-  const quizFrame = quizPage.frames()[1]
-  await quizFrame.waitForSelector('.startquiz')
-  await quizFrame.click('.startquiz')
-  await quizFrame.waitForSelector('#RegisterUnverifiedParticipantForm')
-  await quizFrame.type('#registerunverifiedparticipantform-username', name, {delay: 5})
+  await quizPage.waitForSelector('.startquiz')
+  await quizPage.click('.startquiz')
+  await quizPage.waitForSelector('#RegisterUnverifiedParticipantForm')
+  await quizPage.type('#registerunverifiedparticipantform-username', name, {delay: 5})
   
   // JQuery Error => click two times with a delay
-  await quizFrame.click('#RegisterUnverifiedParticipantForm button')
+  await quizPage.click('#RegisterUnverifiedParticipantForm button')
   await quizPage.waitFor(200)
-  await quizFrame.click('#RegisterUnverifiedParticipantForm button')
+  await quizPage.click('#RegisterUnverifiedParticipantForm button')
 
   // Quizz begin
-  await quizFrame.waitForSelector('.answers')
+  await quizPage.waitForSelector('.answers')
 
   const yeah = async () => {
-    let question = await quizFrame.evaluate(() => document.querySelector('#questioncontainer fieldset header h1').textContent)
+    let question = await quizPage.evaluate(() => document.querySelector('#questioncontainer fieldset header h1').textContent)
     question = question.substring(3, question.length).replace('\n', '').trim()
-    const answer = questions.get(question)
-    await quizFrame.$$eval('.answers li', (nodes, answer) => {
-      const answerElement = nodes.find(node => node.textContent.trim() === answer.trim())
-      if (answerElement) {
-        answerElement.firstChild.firstChild.firstChild.click()
-      }
-    }, answer)
+    if (question !== actualQuestion) {
+      actualQuestion = question
+      const answer = questions.get(question)
+      await quizPage.$$eval('.answers li', (nodes, answer) => {
+        const answerElement = nodes.find(node => node.textContent.trim() === answer.trim())
+        if (answerElement) {
+          answerElement.firstChild.firstChild.firstChild.click()
+        }
+      }, answer)
+    }
   }
 
   while (true) {
     await yeah()
-    if (delay) {
-      await quizPage.waitFor(delay)
-    }
+    await quizPage.waitFor(delay)
   }
 
   // await browser.close()
